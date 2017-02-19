a92774f26147fb4f1f8fe69d2d42ab076d72b79c
luogengxian <luogengxian@lvmama.com>
2017-02-19 11:05:36 +0800

    分销LTS日志操作

--- lts-core/src/main/java/com/github/ltsopensource/biz/logger/es/EsJobLogger.java
+++ lts-core/src/main/java/com/github/ltsopensource/biz/logger/es/EsJobLogger.java
@@ -1,15 +1,10 @@
 package com.github.ltsopensource.biz.logger.es;
 
-import com.github.ltsopensource.admin.response.PaginationRsp;
-import com.github.ltsopensource.biz.logger.JobLogger;
-import com.github.ltsopensource.biz.logger.domain.JobLogPo;
-import com.github.ltsopensource.biz.logger.domain.JobLoggerRequest;
-import com.github.ltsopensource.core.cluster.Config;
-import com.github.ltsopensource.core.commons.utils.CollectionUtils;
-import com.github.ltsopensource.json.JSONObject;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,9 +19,20 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
+
 import com.alibaba.fastjson.JSON;
-
-
+import com.github.ltsopensource.admin.response.PaginationRsp;
+import com.github.ltsopensource.biz.logger.JobLogger;
+import com.github.ltsopensource.biz.logger.domain.JobLogPo;
+import com.github.ltsopensource.biz.logger.domain.JobLoggerRequest;
+import com.github.ltsopensource.core.cluster.Config;
+import com.github.ltsopensource.core.commons.utils.CollectionUtils;
+import com.github.ltsopensource.json.JSONObject;
+/**
+ * 分销LTS日志操作
+ * @author haofeifei
+ *
+ */
 public class EsJobLogger implements JobLogger {
 
 	Logger log = Logger.getLogger(EsJobLogger.class);
@@ -35,11 +41,16 @@ public class EsJobLogger implements JobLogger {
 	
     public EsJobLogger(Config config) {
        
-    }
-
+    }    
+    /**
+     * send post
+     * @param method
+     * @param log
+     * @return
+     */
     public static String doPost(String method,String log){
-    	
-        String urlAPI = "http://super.lvmama.com/dubbo-rest/generic/com.lvmama.prism.biz.esser.ESLtsSyncLogService/"+method;//Post方式没有参数在这里
+		//String urlAPI = "http://localhost:8081/dubbo-rest/generic/com.lvmama.bigger.biz.service.IESLtsSyncLogService/"+method;
+		String urlAPI = "http://super.lvmama.com/dubbo-rest/generic/com.lvmama.prism.biz.esser.IESLtsSyncLogService/"+method;//Post方式没有参数在这里
         String result = "";
         HttpPost httpRequst = new HttpPost(urlAPI);//创建HttpPost对象
         
@@ -48,13 +59,16 @@ public class EsJobLogger implements JobLogger {
 
         List<Map<String, Object>> args = new ArrayList<Map<String, Object>>();
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("type", "com.lvmama.prism.biz.esentity.TntLtsLog");
-        map.put("value", log);
+        //map.put("type", "com.lvmama.prism.biz.esentity.TntLtsLog");
+        map.put("type","java.lang.String");
+        map.put("value",log);
+        
         args.add(map);
-        params.add(new BasicNameValuePair("arguments", JSON.toJSONString(args)));
+        //params.add(new BasicNameValuePair("arguments",JSON.toJSONString(args)));
+        params.add(new BasicNameValuePair("arguments",JSON.toJSONString(args)));
 
         try {
-            httpRequst.setEntity(new UrlEncodedFormEntity(params, CHARACTER_ENCODING));
+            httpRequst.setEntity(new UrlEncodedFormEntity(params,CHARACTER_ENCODING));
             HttpResponse httpResponse = new DefaultHttpClient().execute(httpRequst);
             if(httpResponse.getStatusLine().getStatusCode() == 200)
             {
@@ -72,46 +86,55 @@ public class EsJobLogger implements JobLogger {
         catch (IOException e) {
             e.printStackTrace();
             result = e.getMessage().toString();
+        }finally{
+        	httpRequst.releaseConnection();
         }
         return result;
     }
-    
+    /**
+     *save one log 
+     */
     @Override
     public void log(JobLogPo jobLogPo) {
         if (jobLogPo == null) {
             return;
         }
-        String object = JSONObject.toJSONString(jobLogPo);
-        doPost("saveOne", object);
+        //String object = JSONObject.toJSONString(jobLogPo);
+        doPost("saveOne", encodeMsg(jobLogPo));
     }
     
-
+    /**
+     * save more log
+     */
     @Override
     public void log(List<JobLogPo> jobLogPos) {
         if (CollectionUtils.isEmpty(jobLogPos)) {
             return;
         }
-        String object = com.alibaba.fastjson.JSON.toJSON(jobLogPos).toString();
-        doPost("saveAll", object);
+        //String object = com.alibaba.fastjson.JSON.toJSON(jobLogPos).toString();
+        doPost("saveAll", encodeMsg(jobLogPos));
     }
-
-
-
+    /**
+     * search log informations
+     */
     @Override
     public PaginationRsp<JobLogPo> search(JobLoggerRequest request) {
-
         PaginationRsp<JobLogPo> response = new PaginationRsp<JobLogPo>();
         
-        String objectCount = JSONObject.toJSONString(request);
-        String count = doPost("count", objectCount);
+        Map<String,Object> map = new HashMap<String,Object>();
+    	map.put("pageSize",request.getLimit());
+    	map.put("pageNum",1);//TODO
+    	map.put("parameter",request);
+        //String objectCount = JSONObject.toJSONString(request);
+        String count = doPost("count", encodeMsg(map));
         if (null == count || !count.isEmpty() || Long.valueOf(count) == 0L) {
             return response;
         }
         int cut = Long.valueOf(count).intValue();
         response.setResults(cut);
         
-        String objectData = JSONObject.toJSONString(request);
-		String searchResult = doPost("search", objectData);
+        //String objectData = JSONObject.toJSONString(request);
+		String searchResult = doPost("query", encodeMsg(map));
 		
 		List<JobLogPo> rows = new ArrayList<JobLogPo>();
 		
@@ -124,9 +147,74 @@ public class EsJobLogger implements JobLogger {
 		response.setRows(rows);
         return response;
     }
-
-    
-
+    private static String encodeMsg(Object oj){
+    	if(null==oj){
+    		return "";
+    	}
+    	String result = "";
+    	try {
+    		//json-->encode
+    		result = URLEncoder.encode(JSONObject.toJSONString(oj),CHARACTER_ENCODING);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+    	return result;
+    }    
+    /**
+     * test method
+	 * 要测试dubbo-rest，需要将http://10.200.3.7/svn/soa/trunk/dubbo-rest拉取到本地启动下(检查下zk注册地址是否和需要调用的dubbo服务注册地址相同)
+     * @param args
+     */
+    public static void main(String[] args) throws UnsupportedEncodingException {    	    	
+    	/*List<JobLogPo> jobs = new ArrayList<JobLogPo>();
+    	JobLogPo jlp1 = new JobLogPo();
+    	jlp1.setBizId("saveOneBizId");
+    	jlp1.setBizType("saveOneBizType");
+    	jlp1.setEventType("saveOneEventType");
+    	jlp1.setLogTime(new Date().getTime());
+    	jlp1.setMsg("saveOne.msg.test");
+    	jlp1.setNeedFeedback(true);
+    	jlp1.setPriority(5);
+    	
+    	JobLogPo jlp2 = new JobLogPo();
+    	jlp2.setBizId("44");
+    	jlp2.setBizType("2test");
+    	jlp2.setEventType("2test");
+    	jlp2.setLogTime(new Date().getTime());
+    	jlp2.setMsg("lts.saveAll.test");
+    	jlp2.setNeedFeedback(true);
+    	jlp2.setPriority(5);
+    	
+    	JobLogPo jlp3 = new JobLogPo();
+    	jlp3.setBizId("55");
+    	jlp3.setBizType("btest");
+    	jlp3.setEventType("etest");
+    	jlp3.setLogTime(new Date().getTime());
+    	jlp3.setMsg("lts.saveAll.test");
+    	jlp3.setNeedFeedback(true);
+    	jlp3.setPriority(5);
+    	
+    	jobs.add(jlp1);
+    	System.out.println("saveResult:"+doPost("saveOne",encodeMsg(jlp1)));
+    	
+    	jobs.add(jlp2);
+    	jobs.add(jlp3);    	
+    	System.out.println("saveResult:"+doPost("saveAll",encodeMsg(jobs)));*/
+    	
+    	
+    	JobLoggerRequest jlr = new JobLoggerRequest();
+    	jlr.setBizId("saveOneBizId");
+    	//jlr.setEventType("etest");
+    	
+    	Map<String,Object> map = new HashMap<String,Object>();
+    	//map.put("pageSize",jlr.getLimit());
+    	//map.put("pageNum",1);
+    	map.put("parameter",jlr);
+    	System.out.println("searchParam:"+JSONObject.toJSONString(map));
+    	
+    	System.out.println("searchCout:"+doPost("count",encodeMsg(map)));
+    	System.out.println("searchResult:"+doPost("query",encodeMsg(map)));    	
+	}
    
     /**
     protected void copyProperties(JobLogPo jobLogPo,TntLtsLogVo tntLtsLogVo){
