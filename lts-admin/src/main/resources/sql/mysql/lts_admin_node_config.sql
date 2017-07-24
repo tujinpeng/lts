41b7e95e69b6b5155665bb2b36ecf84a35238218
qiujialong <qiujialong@lvmama.com>
2017-07-24 17:27:35 +0800

    调整sql

--- lts-admin/src/main/resources/sql/mysql/lts_admin_node_config.sql
+++ lts-admin/src/main/resources/sql/mysql/lts_admin_node_config.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `lts_admin_node_config` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `config_name` varchar(255) DEFAULT NULL,
-  `config_value` varchar(255) DEFAULT NULL,
-  `node_type` varchar(255) DEFAULT NULL,
-  `node_group` varchar(255) DEFAULT NULL,
-  `remark` varchar(255) DEFAULT NULL,
+  `config_name` varchar(100) DEFAULT NULL,
+  `config_value` varchar(100) DEFAULT NULL,
+  `node_type` varchar(20) DEFAULT NULL,
+  `node_group` varchar(100) DEFAULT NULL,
+  `remark` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
